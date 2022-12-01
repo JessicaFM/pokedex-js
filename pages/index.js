5ec0ea740f8074f6eb5de27be3a573d61c9ea1eb
Jessica F Martinez <jessicafndez@gmail.com>
2022-12-01 21:03:01 +0100

    starting paginator component

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
-import Cards from '../components/cards.jsx';
 import List from '../components/list.jsx';
+import Paginator from '../components/paginator.jsx';
 import styles from '../styles/Home.module.css'
 
-let limit = 10
-let offset = 0
+let limit = 10 // number by pages
+let offset = 0 // max
 
-function Home({ pokemons }) {
+function Home({ pokemons, total }) {
   return (
-    <List 
-      pokemons = { pokemons } />
+    <>
+      <List 
+        pokemons = { pokemons } 
+      />
+      <Paginator
+        totalPages = { total }
+        currentPage = { offset }
+      />
+    </>
+    
   ) 
 }
 
@@ -16,11 +24,17 @@ export async function getStaticProps()  {
   let url = "https://pokeapi.co/api/v2/pokemon/?limit=" + limit + "&offset=" + offset
   const res = await fetch(url)
   let pokemons = await res.json()
+  let total = 0
+  if(pokemons) {
+    total = pokemons.count
+  }
+
   pokemons = pokemons.results?pokemons.results:null
 
   return {
     props: {
-      pokemons
+      pokemons,
+      total
     }
   }
 }
